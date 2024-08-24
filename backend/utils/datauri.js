7f4f05c266dfc23383f6a9dc1e838c127ac030c8
pavan kumar <pavankumar17552@gmail.com>
2024-08-24 22:44:50 +0530

    conditioon-datauri

--- backend/utils/datauri.js
+++ backend/utils/datauri.js
@@ -2,10 +2,19 @@ import DataUriParser from "datauri/parser.js"
 
 import path from "path";
 
+// const getDataUri = (file) => {
+//     const parser = new DataUriParser();
+//     const extName = path.extname(file.originalname).toString();
+//     return parser.format(extName, file.buffer);
+// }
+
+
 const getDataUri = (file) => {
+    if (!file || !file.originalname) {
+        throw new Error("File or originalname is undefined.");
+    }
     const parser = new DataUriParser();
     const extName = path.extname(file.originalname).toString();
     return parser.format(extName, file.buffer);
 }
-
 export default getDataUri;
