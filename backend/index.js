44cfd47e53292b741ffd3e513657757528d2c082
pavan kumar <pavankumar17552@gmail.com>
2024-08-24 23:28:10 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -16,11 +16,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+// const corsOptions = {
+//     origin:'http://localhost:5173',
+//     credentials:true
+// }
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
-
+    origin: function (origin, callback) {
+        const allowedOrigins = ['http://localhost:5173', 'https://jobportal-hqlg.onrender.com'];
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+};
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
